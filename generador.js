8758b2e396d2d4e9a286224c9d1e7b57218addce
Juan Camilo Acevedo Vargas <camiloacevedovargas@gmail.com>
2026-03-18 20:07:13 -0500

    anexo de retry con 3 intentos

--- generador.js
+++ generador.js
@@ -6,6 +6,34 @@ function generadorCodigo(spec) {
   return `
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+async funtion fetchWithRetry(url, retries = 3) {
+try {
+              const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Request failed");
+  }; 
+
+  return await response.json();
+} catch (error) {
+  if (retries > 0) {
+    console.log("Retrying...");
+    return fetchWithRetry(url, retries - 1);
+  }
+    throw error;
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 async function run() {
   try {
     const response = await fetch("${spec.base_url}${endpoint.path}");
